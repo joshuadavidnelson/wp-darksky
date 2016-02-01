413436e49850fbdf5c4925cbe29513f1c3662368
Joshua Nelson <josh@joshuadnelson.com>
2016-01-31 22:36:43 -0800

    update cache_prefix documentation note

--- wp-forecast-io.php
+++ wp-forecast-io.php
@@ -43,7 +43,7 @@ class Forecast {
 		'latitude'	=> null,
 		'longitude'	=> null,
 		'time'		=> null, // Time in seconds
-		'cache_prefix'	=> 'api_', // careful here, with md5 you are limited to 8 characters before the combined total exceeds the transient name limit
+		'cache_prefix'	=> 'api_', // careful here, md5 is used on the request url to generate the transient name. You are limited to an 8 character prefix before the combined total exceeds the transient name limit
 		'cache_enabled'	=> true,
 		'cache_time'	=> 6 * HOUR_IN_SECONDS, // Time in seconds
 		'clear_cache'	=> false, // set to true to force the cache to clear
